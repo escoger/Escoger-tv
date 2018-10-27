47b88ceef81d25de6104ea0f9630cfe68bd107b7
gaurkuma <gaurav.kumar8@hpe.com>
2018-10-27 13:22:09 +0530

    first commit

---
+++ src/main/java/com/escoger/tv/EscogerTvApplication.java
@@ -0,0 +1,12 @@
+package com.escoger.tv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EscogerTvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EscogerTvApplication.class, args);
+	}
+}
---
+++ src/test/java/com/escoger/tv/EscogerTvApplicationTests.java
@@ -0,0 +1,16 @@
+package com.escoger.tv;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EscogerTvApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
